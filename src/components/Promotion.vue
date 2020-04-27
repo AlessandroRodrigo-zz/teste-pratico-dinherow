214bcab25fb2ce5c6feb94d274053e8dfb9b7fcc
AlessandroRodrigo <alessandro.perez@etec.sp.gov.br>
2020-04-27 17:30:50 -0300

    Otimização do código com o componente Promotion

---
+++ src/components/Promotion.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-layout row class="justify-start align-center my-10 mx-auto">
+    <v-flex>
+      <img
+        src="https://static.netshoes.com.br/bnn/l_netshoes/2020-03-24/4274_deal1.jpg"
+        alt=""
+        class="borderRight cursorPointer"
+      />
+    </v-flex>
+    <v-flex>
+      <img
+        src="https://static.netshoes.com.br/bnn/l_netshoes/2020-03-04/2692_DEAL_desk.jpg"
+        alt=""
+        class="borderRight cursorPointer"
+      />
+    </v-flex>
+    <v-flex>
+      <img
+        src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-05/2134_d1.jpg"
+        alt=""
+        class="borderRight cursorPointer"
+      />
+    </v-flex>
+    <v-flex>
+      <img
+        src="https://static.netshoes.com.br/bnn/l_netshoes/2019-12-05/7895_d4.jpg"
+        alt=""
+        class="cursorPointer"
+      />
+    </v-flex>
+  </v-layout>
+</template>
+
+<style scoped>
+.cursorPointer {
+  cursor: pointer;
+}
+
+.borderRight {
+  border-right: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
