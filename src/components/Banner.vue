14ef2158ed77d55e8a82a006de2efa94c2a0086e
AlessandroRodrigo <alessandro.perez@etec.sp.gov.br>
2020-04-27 17:30:22 -0300

    Otimização do componente Banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-flex>
+    <div style="margin-top: 110px;">
+      <img
+        src="https://static.netshoes.com.br/bnn/l_netshoes/2020-04-24/6597_full1_desk.jpg"
+        alt="banner"
+      />
+    </div>
+  </v-flex>
+</template>
