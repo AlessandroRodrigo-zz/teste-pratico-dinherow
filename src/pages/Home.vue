78d2372d7bae46867adbe2128b6be2afe27d311f
AlessandroRodrigo <alessandro.perez@etec.sp.gov.br>
2020-04-27 17:31:23 -0300

    Otimização do código

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>TESTE ROUTER</div>
+  <div class="d-flex flex-column justify-center">
+    <toolbar />
+    <banner />
+    <promotion />
+    <product />
+  </div>
 </template>
 
 <script>
-export default {};
+import Toolbar from '../components/Toolbar.vue';
+import Banner from '../components/Banner.vue';
+import Promotion from '../components/Promotion.vue';
+import Product from '../components/Product.vue';
+
+export default {
+  components: {
+    Toolbar,
+    Banner,
+    Promotion,
+    Product,
+  },
+};
 </script>
