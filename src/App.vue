78a5b9de519a861c8f71b06d81998a19363e6000
AlessandroRodrigo <alessandro.perez@etec.sp.gov.br>
2020-04-27 17:30:11 -0300

    Persistência de dados no Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <v-app>
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: 'App',
 
   data: () => ({
     //
   }),
+  methods: {
+    ...mapActions(['VERIFY_STORAGE']),
+    verificarStorage() {
+      const storage = sessionStorage.getItem('store');
+
+      const storageJson = JSON.parse(storage);
+
+      if (storage) {
+        this.VERIFY_STORAGE(storageJson);
+      }
+    },
+  },
+  beforeMount() {
+    this.verificarStorage();
+  },
 };
 </script>
